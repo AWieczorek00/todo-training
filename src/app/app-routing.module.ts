73312e53dc254302f82a2fda8a17b03d3eedd2ac
AWieczorek00 <awieczorek0000@gmail.com>
2022-03-01 23:17:50 +0100

    Drugi commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlokPage } from './pages/blok.page';
+import { BlokPageModule } from './pages/blok.page-module';
+
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),BlokPageModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
