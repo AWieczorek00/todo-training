6fe8cba0377c94092e59898bab677c035f8b483d
AWieczorek00 <awieczorek0000@gmail.com>
2022-03-03 19:05:20 +0100

    about-me added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BlokPage } from './pages/blok.page';
 import { BlokPageModule } from './pages/blok.page-module';
+import { AboutMePageModule } from './pages/about-me/about-me.page-module';
+import { AboutUsPageModule } from './pages/about-us.page-module';
 
-
-const routes: Routes = [];
+const routes: Routes = [{ 
+        path: 'omnie', 
+        loadChildren: () => AboutMePageModule
+      },
+  { 
+        path: 'onas', 
+        loadChildren: () => AboutUsPageModule
+      }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes),BlokPageModule],
