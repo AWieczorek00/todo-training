70c8c282c7bff747610b9230253e212a49ceb2d5
AWieczorek00 <awieczorek0000@gmail.com>
2022-03-08 19:17:14 +0100

    content home apge added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutMePageModule } from './pages/about-me/about-me.page-module';
 import { AboutUsPageModule } from './pages/about-us/about-us.page-module';
+import { HomePageModule } from './pages/home/home.page-module';
 
 const routes: Routes = [{ 
         path: 'omnie', 
@@ -10,6 +11,14 @@ const routes: Routes = [{
   { 
         path: 'onas', 
         loadChildren: () => AboutUsPageModule
+      },
+  { 
+        path: 'dom', 
+        loadChildren: () => HomePageModule
+      },
+  { 
+        path: '', 
+        loadChildren: () => HomePageModule
       }
 ];
 
